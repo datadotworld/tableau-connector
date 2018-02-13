cc8e3b98df30078337814459041b30dc5b8ffdbf
Rafael Pereira <rflprr@users.noreply.github.com>
2018-02-13 09:45:00 -0600

    Revert "Convert connector to use native OAuth flow (#33)"
    This reverts commit d9c4d8bbcca46b97005a4383a81878bd467aba2c.

--- src/App.js
+++ src/App.js
@@ -22,12 +22,6 @@ import TableauConnectorForm from './components/TableauConnectorForm'
 import NotTableauView from './components/NotTableauView'
 import TableauConnector from './TableauConnector'
 import queryString from 'query-string'
-import {
-  getToken,
-  getAuthUrl,
-  storeCodeVerifier,
-  removeCodeVerifier
-} from './util'
 
 const tableau = window.tableau
 const connector = new TableauConnector()
@@ -36,9 +30,11 @@ class App extends Component {
 
   constructor () {
     super()
+    this.oauthClientId = process.env.REACT_APP_OAUTH_CLIENT_ID
+    this.oauthRedirectURI = process.env.REACT_APP_OAUTH_REDIRECT_URI
 
     this.parsedQueryString = queryString.parse(location.search)
-    let { dataset_name, query, queryType, token, code } = this.parsedQueryString
+    let { dataset_name, query, queryType, token } = this.parsedQueryString
 
     if (!token) {
       // Only use stored data if returning from auth
@@ -64,18 +60,7 @@ class App extends Component {
     const apiKey = this.getApiKey()
 
     if (!apiKey && this.isTableau) {
-      if (code) {
-        getToken(code)
-        .then(response => {
-          const token = response.data.access_token
-          if (token) {
-            removeCodeVerifier()
-            window.location = `${process.env.REACT_APP_OAUTH_ROOT_URL}?token=${token}`
-          }
-        })
-      } else {
-        this.redirectToAuth()
-      }
+      this.redirectToAuth()
     }
 
     this.state = {
@@ -89,8 +74,7 @@ class App extends Component {
   }
 
   redirectToAuth () {
-    storeCodeVerifier()
-    window.location = getAuthUrl()
+    window.location = `https://data.world/oauth/authorize?client_id=${this.oauthClientId}&redirect_uri=${this.oauthRedirectURI}`
   }
 
   apiKeyHasExpired (apiKey) {
