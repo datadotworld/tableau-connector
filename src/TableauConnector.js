296d048d6d558ca07331bcc05f4f86e21c17c3f2
Rafael Pereira <rflprr@users.noreply.github.com>
2018-07-31 12:46:54 -0500

    Fix "The user has not yet visited the url" error
    Tableau stores the connector URL when a datasource is saved.
    If the user needs to authenticate via OAuth, the original URL will be replaced by the final redirect URL, which contains a single-use auth code we don't want Tableau to persist.
    Using history.pushState() was not a good solution, because Tableau won't trust URLs that the user hasn't effectively visited, which was the reason behind the "The user has not yet visited the url" error.
    To overcome that, we'll replace history.pushState() with a proper redirect, restoring the WDCs canonical URL once the OAuth flow is completed.

--- src/TableauConnector.js
+++ src/TableauConnector.js
@@ -53,7 +53,13 @@ const metadataTable = 'TableColumns'
 
 class TableauConnector {
   constructor (uiCallback, params, code) {
-    this.params = params
+    const connData = JSON.parse(tableau.connectionData || '""')
+    this.params = connData ? {
+      dataset_name: connData.dataset,
+      query: connData.query,
+      queryType: connData.queryType,
+      forceTableau: connData.forceTableau
+    } : params
     this.code = code
 
     this.uiCallback = uiCallback
@@ -73,8 +79,21 @@ class TableauConnector {
     if (this.code && tableau.phase !== tableau.phaseEnum.gatherDataPhase) {
       utils.log('SUCCESS: Authenticate (oauth)')
       const code = this.code
-      this.code = null // Code can only be used once
-      return auth.getToken(code)
+      return auth.getToken(code).then(accessToken => {
+        const {dataset_name, query, queryType, forceTableau} = this.params
+        // Restore canonical WDC URL, which Tableau saves with data source
+        let canonicalUrl = `/?dataset_name=${encodeURIComponent(dataset_name)}`
+        if (query) {
+          canonicalUrl += `&query=${encodeURIComponent(query)}&queryType=${queryType || 'sql'}`
+        }
+        if (forceTableau) {
+          canonicalUrl += `&forceTableau=${forceTableau}`
+        }
+        window.location = canonicalUrl
+
+        // For correctness only. Should never be reached.
+        return accessToken
+      })
     } else {
       const apiKey = auth.getApiKey(true)
       utils.log(`SUCCESS: Authenticate (cached: ${apiKey ? 'hit' : 'miss'})`)
